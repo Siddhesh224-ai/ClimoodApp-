4dee89e4ea6dd822b4fe61ea6b41bd7a46b5d98b
Siddhesh Mutha <siddhesh.24bcs10035@sst.scaler.com>
2025-04-21 23:14:47 +0530

    Initial Commit

---
+++ EndTerm3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EndTerm3
+//
+//  Created by Siddhesh Mutha on 21/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EndTerm3/EndTerm3App.swift
@@ -0,0 +1,17 @@
+//
+//  EndTerm3App.swift
+//  EndTerm3
+//
+//  Created by Siddhesh Mutha on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct EndTerm3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
